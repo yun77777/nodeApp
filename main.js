0a25a935297b4a765e00e6c482479d25a3196c29
yun77777 <victoriousyk@naver.com>
2021-09-11 13:56:19 +0900

    “m”

--- main.js
+++ main.js
@@ -2,44 +2,72 @@ var http = require('http');
 var fs = require('fs');
 var url=require('url');// I'm going to use the module called url
 
+function templateHTML(title,list,body){
+  return `<!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+  var list='<ul>';
+
+  var i=0;
+  while(i<filelist.length){
+    list=list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i=i+1;
+  }
+
+  list=list+'</ul>';
+  return list;
+}
+
+
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData=url.parse(_url, true).query;
-  var title=queryData.id;
-  console.log(queryData.id);
+  var pathname=url.parse(_url, true).pathname;
 
-  console.log(url);
-  if (_url == '/') {
-    title='Welcome';
+
+  if(pathname==='/'){
+    if(queryData.id===undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title='Welcome';
+          var description='Hello, Node.js';
+
+          var list=templateList(filelist);
+          var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
+
+          response.writeHead(200);
+          response.end(template);
+        });
+    } else{
+      fs.readdir('./data', function(error, filelist){
+
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title=queryData.id;
+        var list=templateList(filelist);
+        var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
+
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
   }
-  if (_url == '/favicon.ico') {
+  } else{
     response.writeHead(404);
-    response.end();
-    return;
+    response.end('Not found');
   }
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, 'utf8', function(err, discription){
-    var template=`
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${discription}</p>
-    </body>
-    </html>
-    `;
-    response.end(template);
-  });
+
 
 
 
